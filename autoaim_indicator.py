fe82963115acbe881511ca4be1736e32dfce224f
Krzysztof Chodak <krzysztof.chodak@gmail.com>
2014-11-13 15:14:01 +0100

    20141113

--- autoaim_indicator.py
+++ autoaim_indicator.py
@@ -2,16 +2,12 @@
 AutoAim indicator
 by Krzysztof_Chodak
 """
+import BigWorld, ResMgr, GUI, json, os, time, VehicleGunRotator, Math, math
 from debug_utils import *
+from gui.WindowsManager import g_windowsManager
 from PlayerEvents import g_playerEvents
 from constants import ARENA_PERIOD
-from gui.WindowsManager import g_windowsManager
-import BigWorld
-import ResMgr
-import json
-import os
-import GUI
-from gui import g_guiResetters
+from gui import g_guiResetters, g_repeatKeyHandlers, GUI_SETTINGS
 
 config = None
 old_autoAim = None
@@ -19,14 +15,43 @@ old_onAutoAimVehicleLost = None
 indicator = None
 playerVehicleID = None
 myEventsAttached = False
+toggleKey = 0
+toggleStateOn = True
+enemies = {}
+player = None
+playerVehicle = None
 
-def MYLOGLIVE(message):
+print '2014-11-13'
+
+def MYLOGLIVE(message, permanent_log = True, make_red = True):
     from messenger import MessengerEntry
-    MessengerEntry.g_instance.gui.addClientMessage('<font color="#FF0000">' + message + '</font>')
-    LOG_NOTE(message)
+    if message=="":
+        return
+    if permanent_log:
+        LOG_NOTE(message)
+    if make_red:
+        message = '<font color="#FF0000">' + message + '</font>'
+    MessengerEntry.g_instance.gui.addClientMessage(message)
 
 def MYLOG(message, *args):
     LOG_NOTE(message, *args)
+
+def PT2STR(obj):
+    return "x=%f, y=%f, z=%f" % (obj.x, obj.y, obj.z)
+
+def MYPPRINT(obj, name = None):
+    import inspect, pprint
+    if isinstance(obj, dict) or isinstance(obj, list):
+        pprint.pprint(obj)
+    elif isinstance(obj, Math.Vector3):
+        if name is None:
+            print PT2STR(obj)
+        else:
+            print "%s: %s" % (name, PT2STR(obj))
+    elif hasattr(obj, '__call__'):
+        pprint.pprint(inspect.getargspec(obj))
+    else:
+        pprint.pprint(inspect.getmembers(obj))
 
 def myPe_onArenaPeriodChange(period = ARENA_PERIOD.BATTLE, *args):
     global config
@@ -35,10 +60,9 @@ def myPe_onArenaPeriodChange(period = ARENA_PERIOD.BATTLE, *args):
     global indicator
     global playerVehicleID
     global myEventsAttached
+    global player
     if period is ARENA_PERIOD.BATTLE:
-        #MYLOG('in a battle')
         if g_windowsManager.battleWindow is None:
-            #MYLOG('no battleWindow yet, retrying in a sec')
             BigWorld.callback(1, myPe_onArenaPeriodChange)
             return
         player = BigWorld.player()
@@ -46,12 +70,15 @@ def myPe_onArenaPeriodChange(period = ARENA_PERIOD.BATTLE, *args):
         vehicles = arena.vehicles
         if player.isVehicleAlive:
             playerVehicleID = player.playerVehicleID
-            if not 'SPG' in vehicles[playerVehicleID]['vehicleType'].type.tags:
+            if 'SPG' not in vehicles[playerVehicleID]['vehicleType'].type.tags:
+                for vehicleID, desc in vehicles.items():
+                    if player.team is not desc['team'] and desc['isAlive'] == True:
+                        enemies[vehicleID] = True
+
                 if indicator is None:
                     indicator = TextLabel(config.get('autoaim_indicator_panel', {}))
                     onChangeScreenResolution()
                     new_autoAim(None, True)
-                    #MYLOG('indicator prepared')
                 if not myEventsAttached:
                     old_autoAim = player.autoAim
                     player.autoAim = new_autoAim
@@ -72,9 +99,9 @@ def cleanUp():
     global indicator
     global playerVehicleID
     global myEventsAttached
+    global playerVehicle
     playerVehicleID = None
     if myEventsAttached:
-        player = BigWorld.player()
         player.autoAim = old_autoAim
         old_autoAim = None
         player.onAutoAimVehicleLost = old_onAutoAimVehicleLost
@@ -83,57 +110,73 @@ def cleanUp():
         myEventsAttached = False
     if not indicator is None:
         GUI.delRoot(indicator.window)
-        indicator = None                
-    
+        indicator = None
+    enemies.clear()
+    playerVehicle = None
+    return
+
+
 def new_autoAim(target, init = False):
-    prevAutoAimVehicle = None
-    if not init:
-        prevAutoAimVehicle = BigWorld.player().autoAimVehicle
+    global playerVehicle
+    global counter
+    prevAutoAimVehicleID = 0
+    if init:
+        playerVehicle = BigWorld.entity(playerVehicleID)
+    else:
+        prevAutoAimVehicleID = player._PlayerAvatar__autoAimVehID
         old_autoAim(target)
-    autoAimVehicle = BigWorld.player().autoAimVehicle
-    enabled = not autoAimVehicle is None
-    #MYLOGLIVE("autoAimVehicle = %s" % (not autoAimVehicle is None))
-    if enabled and config.get("use_target_as_text", False):
-        indicator.setText(autoAimVehicle.typeDescriptor.type.userString)
+    autoAimVehicleID = player._PlayerAvatar__autoAimVehID
+    enabled = autoAimVehicleID != 0
+    if enabled and config.get('use_target_as_text', False):
+        indicator.setText(BigWorld.entity(autoAimVehicleID).typeDescriptor.type.shortUserString[0:config.get('max_characters', 15) - 1])
     indicator.setVisible(enabled)
-    if not init and prevAutoAimVehicle is None and not enabled and config.get("snap_to_nearest", False):
-        player = BigWorld.player()
-        source = player.inputHandler.getDesiredShotPoint()
-        new_target = None
-        distance_to_target = 10000
-        vehicles = player.arena.vehicles
-        for vehicleID, desc in vehicles.items():
-            if player.team is not vehicles[vehicleID]['team'] and desc['isAlive'] == True:
-                entity = BigWorld.entity(vehicleID)
-                if not entity is None:
-                    vehicle = BigWorld.entities[vehicleID]
-                    distance = source.flatDistTo(vehicle.position)
+    if not init and prevAutoAimVehicleID == 0 and not enabled and config.get("snap_to_nearest", False):
+        desiredShotPoint = player.inputHandler.getDesiredShotPoint()
+        if desiredShotPoint is None:
+            MYLOG('No desiredShotPoint available - trying alternative')
+            desiredShotPoint = player.gunRotator.markerInfo[0]
+        if desiredShotPoint is None:
+            MYLOG('No desiredShotPoint available')
+        else:
+            new_target = None
+            distance_to_target = math.radians(config.get("snapping_angle_degrees", 7.5))
+            vehicles = player.arena.vehicles
+            v1norm = normalize(desiredShotPoint - playerVehicle.position)
+            for vehicleID in enemies:
+                vehicle = BigWorld.entity(vehicleID)
+                if vehicle is not None and vehicle.isAlive():
+                    v2norm = normalize(vehicle.position - playerVehicle.position)
+                    distance = math.acos(v1norm.x * v2norm.x + v1norm.y * v2norm.y + v1norm.z * v2norm.z)
+                    if distance < 0:
+                        distance = -distance
+                    if distance > math.pi:
+                        distance = 2 * math.pi - distance
                     if distance < distance_to_target:
                         new_target = vehicle
                         distance_to_target = distance
-        if not new_target is None:
-            new_autoAim(new_target)
+
+            if new_target is not None:
+                new_autoAim(new_target)
+
+def normalize(v):
+    return v / math.sqrt(v.x * v.x + v.y * v.y + v.z * v.z)
+
 
 def new_onAutoAimVehicleLost():
     old_onAutoAimVehicleLost()
     indicator.setVisible(False)
 
+
 def myOnVehicleKilled(vehicleID, *args):
-    global playerVehicleID
-    global indicator
-    #MYLOGLIVE('myOnVehicleKilled (%s, own %s)' % (vehicleID, playerVehicleID))
     if vehicleID == playerVehicleID:
-        playerVehicleID = None
-        #MYLOGLIVE("Oh no! We've got killed :(")
-        GUI.delRoot(indicator.window)
-        indicator = None
-    
-# borrowed from https://github.com/macrosoft/wothp/blob/master/src/totalhp.py
+        cleanUp()
+
+
 class TextLabel(object):
     label = None
     shadow = None
     window = None
-    color = '\cFFFFFFFF;'
+    color = '\\cFFFFFFFF;'
     visible = True
     x = 0
     y = 0
@@ -143,24 +186,24 @@ class TextLabel(object):
 
     def __init__(self, config):
         if config.get('color', False):
-            self.color = '\c' + config.get('color')[1:] + 'FF;'
+            self.color = '\\c' + config.get('color')[1:] + 'FF;'
         self.visible = config.get('visible', True)
-        self.x  = config.get('x', 0)
-        self.y  = config.get('y', 0)
-        self.hcentered  = config.get('hcentered', False)
-        self.vcentered  = config.get('vcentered', False)
-        background = os.path.join('scripts', 'client', 'mods', config.get('background')) \
-            if config.get('background', '') else ''
+        self.x = config.get('x', 0)
+        self.y = config.get('y', 0)
+        self.hcentered = config.get('hcentered', False)
+        self.vcentered = config.get('vcentered', False)
+        background = os.path.join('scripts', 'client', 'mods', config.get('background')) if config.get('background', '') else ''
         self.window = GUI.Window(background)
-        self.window.materialFX = "BLEND"
-        self.window.verticalAnchor = "TOP"
-        self.window.horizontalAnchor = "LEFT"
+        self.window.materialFX = 'BLEND'
+        self.window.verticalAnchor = 'TOP'
+        self.window.horizontalAnchor = 'LEFT'
         self.window.horizontalPositionMode = 'PIXEL'
         self.window.verticalPositionMode = 'PIXEL'
         self.window.heightMode = 'PIXEL'
         self.window.widthMode = 'PIXEL'
         self.window.width = config.get('width', 186)
         self.window.height = config.get('height', 32)
+        self.autoSize = True
         GUI.addRoot(self.window)
         self.shadow = GUI.Text('')
         font = config.get('font', 'default_medium.font')
@@ -173,11 +216,11 @@ class TextLabel(object):
     def installItem(self, item, font):
         item.font = font
         self.window.addChild(item)
-        item.verticalAnchor = "TOP"
-        item.horizontalAnchor = "CENTER"
+        item.verticalAnchor = 'CENTER'
+        item.horizontalAnchor = 'CENTER'
         item.horizontalPositionMode = 'PIXEL'
         item.verticalPositionMode = 'PIXEL'
-        item.position = (self.window.width/2, 0, 1)
+        item.position = (self.window.width / 2, self.window.height / 2, 1)
         item.colourFormatting = True
 
     def setVisible(self, flag):
@@ -187,20 +230,50 @@ class TextLabel(object):
         self.label.visible = flag
 
     def setText(self, text, color = None):
-        shadowText = text.replace('\c60FF00FF;','')
-        self.shadow.text = '\c000000FF;' + shadowText
-        color = '\c' + color + 'FF;' if color else self.color
+        shadowText = text.replace('\\c60FF00FF;', '')
+        self.shadow.text = '\\c000000FF;' + shadowText
+        color = '\\c' + color + 'FF;' if color else self.color
         self.label.text = color + text
+
 
 def onChangeScreenResolution():
     sr = GUI.screenResolution()
     for panel in [indicator]:
         if panel is None:
             continue
-        x = sr[0]/2 - panel.window.width /2 + panel.x if panel.hcentered else panel.x
-        y = sr[1]/2 - panel.window.height/2 + panel.y if panel.vcentered else panel.y
+        x = sr[0] / 2 - panel.window.width / 2 + panel.x if panel.hcentered else panel.x
+        y = sr[1] / 2 - panel.window.height / 2 + panel.y if panel.vcentered else panel.y
         panel.window.position = (x, y, 1)
 
+    return
+
+
+def myHandleRepeatKeyEvent(event):
+    global toggleStateOn
+    if GUI_SETTINGS.minimapSize:
+        if event.key == toggleKey and event.repeatCount == 1:
+            if toggleStateOn:
+                toggleStateOn = False
+                g_playerEvents.onArenaPeriodChange -= myPe_onArenaPeriodChange
+                cleanUp()
+                MYLOGLIVE(config.get('toggledOffMsg', ''), make_red=False)
+            else:
+                toggleStateOn = True
+                g_playerEvents.onArenaPeriodChange += myPe_onArenaPeriodChange
+                if g_windowsManager.battleWindow is not None:
+                    myPe_onArenaPeriodChange()
+                MYLOGLIVE(config.get('toggledOnMsg', ''), make_red=False)
+            config['toggleStateOn'] = toggleStateOn
+            with open(conf_file, 'w') as data_file:
+                try:
+                    json.dump(config, data_file, sort_keys=True, indent=4, separators=(',', ': '))
+                except:
+                    print 'Error while saving %s: %s' % (conf_file, sys.exc_info()[0])
+
+    return
+
+
+conf_file = None
 res = ResMgr.openSection('../paths.xml')
 sb = res['Paths']
 vals = sb.values()[0:2]
@@ -213,10 +286,16 @@ for vl in vals:
                 try:
                     config = json.load(data_file)
                 except:
-                    print "Error while loading autoaim_indicator.json: %s" % sys.exc_info()[0]
+                    import sys
+                    print 'Error while loading %s: %s' % (conf_file, sys.exc_info()[0])
                 finally:
                     break
 
 if config:
-    g_playerEvents.onArenaPeriodChange += myPe_onArenaPeriodChange
-    g_guiResetters.add(onChangeScreenResolution)
+    toggleKey = config['toggleKeyCode']
+    if toggleKey > 0:
+        g_repeatKeyHandlers.add(myHandleRepeatKeyEvent)
+        toggleStateOn = config['toggleStateOn']
+    if toggleStateOn:
+        g_playerEvents.onArenaPeriodChange += myPe_onArenaPeriodChange
+        g_guiResetters.add(onChangeScreenResolution)
